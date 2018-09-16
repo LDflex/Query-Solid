b8d5a10cc82d5adab07563c99fbdbc1890404d60
Ruben Verborgh <ruben@verborgh.org>
2018-09-16 16:20:42 +0200

    Add profile demo.

---
+++ demo/profile.js
@@ -0,0 +1,17 @@
+const { node } = require('../lib').default;
+
+const ruben = node('https://ruben.verborgh.org/profile/#me');
+showProfile(ruben);
+
+async function showProfile(person) {
+  const label = await person.label;
+  console.log(`\nNAME: ${label}`);
+
+  console.log('\nTYPES');
+  for await (const type of person.type)
+    console.log(`  - ${type}`);
+
+  console.log('\nFRIENDS');
+  for await (const name of person.friends.firstName)
+    console.log(`  - ${name} is a friend`);
+}
