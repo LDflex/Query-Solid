63b6bde6bcf48727363191c18a3b30c6a3a1a336
Ruben Verborgh <ruben@verborgh.org>
2018-10-19 23:02:08 +0200

    Upgrade ldflex to 1.0.0.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import { QueryPath, QueryPathFactory } from 'ldflex';
+import { PathFactory } from 'ldflex';
 import ComunicaEngine from 'ldflex-comunica';
 import auth from 'solid-auth-client';
 import context from './context';
 
-const factory = new QueryPathFactory({ context });
+const nodePaths = new PathFactory({ context });
+
+const userPaths = new PathFactory({
+  resolvers: [{
+    supports: () => true,
+    resolve: property => node(getWebId())[property],
+  }],
+});
 
 /**
  * Starts a query path from the given subject
  */
 export function node(subject) {
   const queryEngine = new ComunicaEngine(subject);
-  return factory.create({ queryEngine }, { subject });
+  return nodePaths.create({ queryEngine }, { subject });
 }
 
 /**
  * Starts a query path from the currently logged in user
  */
-export const user = new QueryPath({
-  resolvers: [{
-    supports() { return true; },
-    resolve(path, property) {
-      // Obtain the user's WebID
-      const webId = auth.currentSession().then(session => {
-        if (!session)
-          throw new Error('Path expression with "user" failed: no active user');
-        return session.webId;
-      });
-      // Start a new path from the user
-      return node(webId)[property];
-    },
-  }],
-});
+export const user = userPaths.create();
+
+/**
+ * Gets the WebID of the logged in user.
+ */
+async function getWebId() {
+  const session = await auth.currentSession();
+  if (!session)
+    throw new Error('Path expression with "user" failed: no active user');
+  return session.webId;
+}
