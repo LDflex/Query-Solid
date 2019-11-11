41673cb767b89ed64d2fcb6e90ae9c20ff47b4bc
Ruben Verborgh <ruben@verborgh.org>
2019-11-11 20:50:01 +0100

    Add preload.

--- test.js
+++ test.js
@@ -1,17 +1,25 @@
 const { default: data } = require('.');
 const solid = { data };
-const { asyncMap } = require('iter-tools');
 
 (async function() {
   let container = solid.data["http://localhost:8080/users.jsonld"];
+
+  console.time('fetch');
   await container.ldp_contains;
+  console.timeEnd('fetch');
+
+  const items = container.ldp_contains;
   for (var i=0; i < 10; i++) {
-    console.time(container.toString())
-    for await (const r of container.ldp_contains) {
+    console.time('preload')
+    await items.preload('rdfs:label', 'http://happy-dev.fr/owl/#username', 'http://happy-dev.fr/owl/#email');
+    console.timeEnd('preload')
+
+    console.time('items')
+    for await (const r of items) {
       await r['rdfs:label']
       await r['http://happy-dev.fr/owl/#username']
       await r['http://happy-dev.fr/owl/#email']
     }
-    console.timeEnd(container.toString())
+    console.timeEnd('items')
   }
 }())
