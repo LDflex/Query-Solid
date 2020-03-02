fca6f1b01aefacea014ef682913aa4ca7b812a0e
Ruben Verborgh <ruben@verborgh.org>
2020-03-02 01:07:11 +0100

    Use new @ldflex/comunica package.

--- src/exports/comunica.js
+++ src/exports/comunica.js
@@ -1,6 +1,6 @@
 import RootPath from '../RootPath';
 import SolidUpdateEngine from '../SolidUpdateEngine';
-import ComunicaEngine from 'ldflex-comunica';
+import ComunicaEngine from '@ldflex/comunica';
 
 // Export the root path with Comunica as query engine
 export default new RootPath({
